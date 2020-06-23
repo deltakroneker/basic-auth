943b681510057be280cde84664c4391099c17c29
deltakroneker <nikola.milic.ios@gmail.com>
2020-06-23 12:42:37 +0200

    Add show, destroy, update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,13 @@ class UsersController < ApplicationController
     render json: user, status: :ok
   end
 
+  def show
+    user = User.find(params[:id])
+    render json: user, status: :ok
+  end
+
   def create
     user = User.new(user_params)
-
     if user.save
       render json: { message: "User created successfully" }, status: :created
     else
@@ -14,12 +18,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: { message: "User deleted successfully" }, status: :ok
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render json: { message: "User updated successfully" }, status: :ok
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   def login
     user = User.find_by(email: params[:email].to_s.downcase)
-
     if user && user.authenticate(params[:password])
       auth_token = JsonWebToken.encode({ user_id: user.id })
-      render json: { auth_token: auth_token }, status: :ok
+      render json: { user: user, auth_token: auth_token }, status: :ok
     else
       render json: { error: "Invalid username / password" }, status: :unauthorized
     end
