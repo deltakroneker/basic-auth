d471870304b6f72c2498ff2179834b0e480f851f
deltakroneker <nikola.milic.ios@gmail.com>
2020-06-23 13:43:59 +0200

    Add admin flag to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_211442) do
+ActiveRecord::Schema.define(version: 2020_06_23_113619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_06_22_211442) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
